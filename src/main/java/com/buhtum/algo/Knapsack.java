808047f2606c1577642c2230b9f40c89403efef3
Svilen Ivanov <svilen.ivanov@gmail.com>
2014-11-12 11:57:51 +0200

    Initial (not working) implementation of Knapsack algorithm

--- src/main/java/com/buhtum/algo/Knapsack.java
+++ src/main/java/com/buhtum/algo/Knapsack.java
@@ -1,7 +1,33 @@
 package com.buhtum.algo;
 
+import org.javatuples.Pair;
+
+import java.util.List;
+
 public class Knapsack {
-    public int test() {
+    private final int capacity;
+
+    public Knapsack(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int calculateMaxValue(List<Pair<Integer, Integer>> cakeTuples) {
+        int m[][] = new int[cakeTuples.size()][capacity];
+        for (int i = 0; i < capacity; i++) {
+            m[0][i] = 0;
+        }
+        for (int i = 1; i < cakeTuples.size(); i++) {
+            for (int j = 0; i <= capacity; j++) {
+                int weight = cakeTuples.get(i).getValue0();
+                int value = cakeTuples.get(i).getValue1();
+                if (weight <= j) {
+                    m[i][j] = Math.max(m[i - 1][j], m[i - 1][j - weight] + value);
+                } else {
+                    m[i][j] = m[i - 1][j];
+                }
+            }
+        }
+
         return 1;
     }
 }
--- src/test/java/KnapsackTest.java
+++ src/test/java/KnapsackTest.java
@@ -1,13 +1,25 @@
 import com.buhtum.algo.Knapsack;
+import com.google.common.collect.ImmutableList;
+import org.javatuples.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 @Test
 public class KnapsackTest {
     private final static Logger log = LoggerFactory.getLogger(KnapsackTest.class);
+
     public void test() {
-        Knapsack knapsack = new Knapsack();
-        log.debug("Output is {}", knapsack.test());
+        List<Pair<Integer, Integer>> cakeTuples = ImmutableList.of(
+                Pair.with(7, 160),
+                Pair.with(3, 90),
+                Pair.with(2, 15));
+
+        int capacity = 20;
+
+        Knapsack knapsack = new Knapsack(capacity);
+        log.debug("Max value for {} is {}", cakeTuples, knapsack.calculateMaxValue(cakeTuples));
     }
 }
