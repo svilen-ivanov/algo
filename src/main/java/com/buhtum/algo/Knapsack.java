8b9e9b36b002160b13e6962e90c70758c0f70e91
Svilen Ivanov <svilen.ivanov@gmail.com>
2014-11-13 16:19:03 +0200

    Implemented naive and dynamic 0-1 Knapsack algorithm

--- src/main/java/com/buhtum/algo/Knapsack.java
+++ src/main/java/com/buhtum/algo/Knapsack.java
@@ -1,17 +1,18 @@
 package com.buhtum.algo;
 
 import org.javatuples.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
 import java.util.List;
 
+import static java.lang.Math.*;
+
 public class Knapsack {
-    private final int capacity;
+    private final static Logger log = LoggerFactory.getLogger(Knapsack.class);
 
-    public Knapsack(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public int calculateMaxValue(List<Pair<Integer, Integer>> cakeTuples) {
+    public static int calculateMaxValue(int capacity, List<Pair<Integer, Integer>> cakeTuples) {
         int m[][] = new int[cakeTuples.size()][capacity];
         for (int i = 0; i < capacity; i++) {
             m[0][i] = 0;
@@ -21,7 +22,7 @@ public class Knapsack {
                 int weight = cakeTuples.get(i).getValue0();
                 int value = cakeTuples.get(i).getValue1();
                 if (weight <= j) {
-                    m[i][j] = Math.max(m[i - 1][j], m[i - 1][j - weight] + value);
+                    m[i][j] = max(m[i - 1][j], m[i - 1][j - weight] + value);
                 } else {
                     m[i][j] = m[i - 1][j];
                 }
@@ -29,5 +30,52 @@ public class Knapsack {
         }
 
         return 1;
+    }
+
+    /**
+     * Recursive implementation of 0-1 knapsack problem. Terrible O(2^n)
+     */
+    public static int naiveRecursive01(List<Pair<Integer, Integer>> tuples, int capacity) {
+        return naiveRecursive01(tuples, capacity, tuples.size());
+    }
+
+    private static int naiveRecursive01(List<Pair<Integer, Integer>> tuples, int capacity, int size) {
+        if (capacity == 0 || size == 0) return 0;
+
+        final int index = size - 1;
+        final Pair<Integer, Integer> item = tuples.get(index);
+        final int value = item.getValue0();
+        final int weight = item.getValue1();
+
+        if (weight > capacity) {
+            return naiveRecursive01(tuples, capacity, index);
+        } else {
+            return max(
+                    value + naiveRecursive01(tuples, capacity - weight, index),
+                    naiveRecursive01(tuples, capacity, index));
+        }
+    }
+
+    public static int dynamic01(List<Pair<Integer, Integer>> tuples, int capacity) {
+        final int size = tuples.size();
+        int weights[][] = new int[size][capacity + 1];
+
+        Arrays.fill(weights[0], 0);
+
+        for (int i = 1; i < size; i++) {
+            final Pair<Integer, Integer> item = tuples.get(i);
+            final int value = item.getValue0();
+            final int weight = item.getValue1();
+
+            for (int w = 0; w <= capacity; w++) {
+                if (weight <= w) {
+                    weights[i][w] = max(weights[i - 1][w], value + weights[i - 1][w - weight]);
+                } else {
+                    weights[i][w] = weights[i - 1][w];
+                }
+            }
+        }
+
+        return weights[size - 1][capacity];
     }
 }
--- src/test/java/KnapsackTest.java
+++ src/test/java/KnapsackTest.java
@@ -7,19 +7,32 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 @Test
 public class KnapsackTest {
     private final static Logger log = LoggerFactory.getLogger(KnapsackTest.class);
 
-    public void test() {
-        List<Pair<Integer, Integer>> cakeTuples = ImmutableList.of(
-                Pair.with(7, 160),
-                Pair.with(3, 90),
-                Pair.with(2, 15));
+    public static final List<Pair<Integer, Integer>> ITEMS = ImmutableList.of(
+            Pair.with(60, 10),
+            Pair.with(100, 20),
+            Pair.with(100, 60),
+            Pair.with(120, 30));
+    public static final int CAPACITY = 50;
+    public static final int EXPECTED_MAX_VALUE = 220;
 
-        int capacity = 20;
+    public void naive() {
+        // http://www.geeksforgeeks.org/dynamic-programming-set-10-0-1-knapsack-problem/
+        final int naiveMaxValue = Knapsack.naiveRecursive01(ITEMS, CAPACITY);
+        log.debug("Naive max value for {} is {}", ITEMS, naiveMaxValue);
+        assertThat(naiveMaxValue, is(EXPECTED_MAX_VALUE));
+    }
 
-        Knapsack knapsack = new Knapsack(capacity);
-        log.debug("Max value for {} is {}", cakeTuples, knapsack.calculateMaxValue(cakeTuples));
+    
+    public void dynamic() {
+        final int dynamicMaxValue = Knapsack.dynamic01(ITEMS, CAPACITY);
+        log.debug("Dynamic max value for {} is {}", ITEMS, dynamicMaxValue);
+        assertThat(dynamicMaxValue, is(EXPECTED_MAX_VALUE));
     }
 }
